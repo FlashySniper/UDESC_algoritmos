558f3aa9e80afd63c29676dba88c3d673f1e1a0a
Gabriel L Soares <gabri.soares007@gmail.com>
2023-11-01 10:48:44 -0300

    update

--- Trabalho2/main.c
+++ Trabalho2/main.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main(int argc, char * argv[]) {
-  int a, b, i;
+  int a, b;
   int temp; //variavel temporaria
-  int n=2, m=2; //contadores
-  int mmc=1, mdc=1;
+  int n=2; //contador
+  int mmc=1;
+  int mdc=1;
   int sair; //numero para continuar o loop
+  printf("-----MMC e MDC de numeros inteiros-----\n");
   do{
   	do{
   	printf("Digite o primeiro numero: ");
@@ -17,10 +20,11 @@ int main(int argc, char * argv[]) {
   	scanf("%d", &b);
   	}while(b<=0); 
   	while(a != 1 && b != 1){
-    	if ((a % n == 0) && (b % n == 0)){
+		if (a % n == 0 && b % n == 0){
     		a = a / n;
     		b = b / n;
-			mmc = mmc * n;  
+			mmc = mmc * n;
+			mdc = mdc * n; 
     	}else{
     		if (a % n == 0){
     			a = a / n;
@@ -28,17 +32,22 @@ int main(int argc, char * argv[]) {
 			}else{
 				if (b % n == 0){
     			b = b / n;
-				mmc = mmc * n; 
+				mmc = mmc * n;
 				}else{
 					n = n+1;
+					mmc = mmc * n;
 				}
 			}	
-  		}	
+		}		
 	}
-  	printf("MMC: %d\n", mmc);
-  	printf("MDC: %d\n", mdc);
-  	printf("Digite 1 para continuar ou 0 para finalizar o programa: ");
-  	scanf("%d", &sair);
+	mmc = mmc * n;
+    printf("MMC: %d\n", mmc);
+    printf("MDC: %d\n", mdc);
+    mdc = 1;
+	mmc = 1;
+	n = 2;
+    printf("Digite 1 para continuar ou 0 para finalizar o programa: ");
+    scanf("%d", &sair);
 }while(sair != 0);
 return 0;
 }
