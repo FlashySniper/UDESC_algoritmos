32d6ff800b471e0e41dcfcf0edbbc20e42158c4a
Gabriel L Soares <gabri.soares007@gmail.com>
2023-11-22 20:36:27 -0300

    Create trabalho3.c

---
+++ Trabalho3/trabalho3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char * argv[]){
+	do{
+		int opcao = 0;
+		printf("Selecione a opção que deseja utilizar: ")
+		scanf("%d", &opcao)
+		switch (opcao){
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			case 6:
+				break;
+			default:
+				printf("Essa opção não existe!")
+		}
+	}while(opcao != 7);
+
+	return 0;
+}
